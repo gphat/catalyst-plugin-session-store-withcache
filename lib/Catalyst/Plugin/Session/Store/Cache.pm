ce206f829b35ea1d09316224cfc91b07080920e8
Cory Watson <github@onemogin.com>
2009-04-11 00:49:36 -0500

    Docs.

--- lib/Catalyst/Plugin/Session/Store/Cache.pm
+++ lib/Catalyst/Plugin/Session/Store/Cache.pm
@@ -16,6 +16,34 @@ our $VERSION = '0.01';
 
 __PACKAGE__->mk_classdata(qw/_session_cache/);
 
+=head1 NAME
+
+Catalyst::Plugin::Session::Store::Cache - Caching layer for Catalyst Sessions
+
+=head1 SYNOPSIS
+
+    use Catalyst qw/Session Session::Store::Cache Session::Store::Something/;
+
+    MyApp->config->{session} = {
+        cache => {
+            driver => 'Memory',
+            seconds => 300
+        }
+    };
+
+=head1 DESCRIPTION
+
+C<Catalyst::Plugin::Session::Store::Cache> sits atop your existing session
+store, caching sessions reads and writes.  The C<seconds> configuration option
+provides the number of seconds that the session is allowed to be "stale".
+Failure of the underlying cache during this time would result in lost data,
+but that might be ok with you if it speeds things up.
+
+L<CHI> is used as the underlying cache, so the C<driver> option must be
+a driver that CHI will recognize.
+
+=cut
+
 sub setup_session {
     my $c = shift;
 
@@ -24,7 +52,7 @@ sub setup_session {
     $c->maybe::next::method(@_);
 
     my $cache = CHI->new(
-        driver => $c->config->{session}->{cache}->{driver}
+        driver => $c->config->{session}->{cache}->{driver} || 'Memory'
     );
 
     $c->_session_cache($cache);
@@ -66,7 +94,7 @@ sub store_session_data {
         # If we are asked to store an expiration, compare it to the existing
         # expiration and only let it go to the backend if the expire time
         # is far enough, based on the config
-        my $fudge = $c->config->{session}->{cache}->{time} || 10;
+        my $fudge = $c->config->{session}->{cache}->{seconds} || 10;
         if($data > ($curr_expiry + $fudge)) {
             # The new expiration is greater than the current one plus the
             # fudge time so let it go to the real store
@@ -102,5 +130,22 @@ sub delete_expired_session {
 
     $c->maybe::next::method(@_);
 }
+
+=head1 AUTHOR
+
+Cory G Watson, C<< <gphat at cpan.org> >>
+
+=head1 ACKNOWLEDGEMENTS
+
+Florian Ragwitz for providing some hints on implementation.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Cory G Watson, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
 
 1;
